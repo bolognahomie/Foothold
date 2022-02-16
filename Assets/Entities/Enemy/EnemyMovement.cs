148c43a1c5f8c3b900ebbe364bb8631475db4477
loganbru <loganbrunnerr@gmail.com>
2022-02-15 19:17:11 -0500

    added enemy speed variable and dynamic orientation

--- Assets/Entities/Enemy/EnemyMovement.cs
+++ Assets/Entities/Enemy/EnemyMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
   [SerializeField] List<Waypoint> path = new List<Waypoint>();
-  [SerializeField] float waitTime = 1f;
+  [SerializeField] float movementSpeed = 1f;
 
   void Start()
   {
@@ -20,9 +20,12 @@ public class EnemyMovement : MonoBehaviour
       Vector3 endPosition = waypoint.transform.position; // identify end tile, tile we want to move our entity to next
       float travelPercent = 0f;
 
+      transform.LookAt(endPosition); // makes object point towards next tile which keeps our entity pointing the correct way
+
       while (travelPercent < 1) // while the travel percent is less than 1, i.e. not yet at the end tile (Travel percent is the percent currently traveled between the start and end tiles, 0-1)
       {
-        travelPercent += Time.deltaTime; // update travelpercent with time.deltaTime
+        travelPercent += Time.deltaTime * movementSpeed; // update travelpercent with time.deltaTime multiplied by our movement speed
+        Debug.Log($"Travel Percent: {travelPercent} // time.DeltaTime: {Time.deltaTime}");
         transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent); // moves entity towards next tile
         yield return new WaitForEndOfFrame(); // yeild back until the end of the current frame
       }
