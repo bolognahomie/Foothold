e8878b9dfe17a1b8d1ebb2508fa65510e437b3f9
loganbru <loganbrunnerr@gmail.com>
2022-02-15 19:07:27 -0500

    smoothed enemy movement, redesigned main path

--- Assets/Entities/Enemy/EnemyMovement.cs
+++ Assets/Entities/Enemy/EnemyMovement.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+  [SerializeField] List<Waypoint> path = new List<Waypoint>();
+  [SerializeField] float waitTime = 1f;
 
-    void Start()
-    {
-        PrintWaypointName();
-    }
+  void Start()
+  {
+    StartCoroutine(FollowPath()); // start coroutine
+  }
 
-    void PrintWaypointName()
+  IEnumerator FollowPath()
+  {
+    foreach (Waypoint waypoint in path) // for loop for each tile in the path, waypoint = the tile we are currently moving to
     {
-        foreach(Waypoint waypoint in path)
-        {
-            Debug.Log(waypoint.name);
-        }
+      Vector3 startPosition = transform.position; // identify start tile, tile our entity is currently on
+      Vector3 endPosition = waypoint.transform.position; // identify end tile, tile we want to move our entity to next
+      float travelPercent = 0f;
+
+      while (travelPercent < 1) // while the travel percent is less than 1, i.e. not yet at the end tile (Travel percent is the percent currently traveled between the start and end tiles, 0-1)
+      {
+        travelPercent += Time.deltaTime; // update travelpercent with time.deltaTime
+        transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent); // moves entity towards next tile
+        yield return new WaitForEndOfFrame(); // yeild back until the end of the current frame
+      }
     }
+  }
 }
